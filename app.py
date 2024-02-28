9a9d202d0c82034fb45bfad650f7c69a741254b4
adiintify <adityanama97@gmail.com>
2024-02-28 08:52:07 +0530

    Vulnerable Python App

---
+++ app.py
@@ -0,0 +1,17 @@
+import pickle
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# A simple route to deserialize data received over the network
+
+
+@app.route('/unpickle', methods=['POST'])
+def unpickle():
+    data = request.data  # WARNING: This is unsafe!
+    obj = pickle.loads(data)  # Insecure deserialization
+    return "Object deserialized!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
