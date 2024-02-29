4d7691a2e09a62aa5c8ddafd2a69ea33cb3c2dd7
adiintify <adityanama97@gmail.com>
2024-02-29 16:30:41 +0530

    Minor Change

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
-import pickle
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# A simple route to deserialize data received over the network
-
-
 @app.route('/unpickle', methods=['POST'])
-def unpickle():
-    data = request.data  # WARNING: This is unsafe!
-    obj = pickle.loads(data)  # Insecure deserialization
-    return "Object deserialized!"
-
+def safe_method():
+    data = request.json  # Use JSON data
+    return jsonify(data)  # Return JSON response
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app_sql.py
+++ app_sql.py
@@ -3,17 +3,17 @@ import sqlite3
 
 app = Flask(__name__)
 
-# WARNING: This route is vulnerable to SQL Injection
 @app.route('/search', methods=['GET'])
 def search():
     user_input = request.args.get('name')
-    # Vulnerable query
-    query = f"SELECT * FROM users WHERE name = '{user_input}'"
+    # Secure query using parameterized statements
+    query = "SELECT * FROM users WHERE name = ?"
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute(query)
+    cursor.execute(query, (user_input,))
     result = cursor.fetchall()
     return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
