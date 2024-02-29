a10b0f19478e2d928fc6063d693456c9cea352bd
adiintify <adityanama97@gmail.com>
2024-02-29 17:51:38 +0530

    Minor Change

--- source.py
+++ source.py
@@ -12,18 +12,29 @@ headers = {
     'Accept': 'application/vnd.github.v3+json',
 }
 
-response = requests.get(f'https://api.github.com/repos/{username}/{repo}/code-scanning/alerts', headers=headers)
+response = requests.get(
+    f'https://api.github.com/repos/{username}/{repo}/code-scanning/alerts', headers=headers)
 
 alerts = response.json()
 
-high_severity_alerts = [alert for alert in alerts if alert['rule']['severity'].lower() == 'high']
+high_severity_alerts = [
+    alert for alert in alerts if alert['rule']['severity'].lower() == 'high']
 
-print("High severity alerts:")
+# high_severity_alerts = [
+#     {'severity': 'high', 'rule': {'id': 'CWE-200', 'name': 'Test Vulnerability'}}
+# ]
+
 for alert in high_severity_alerts:
     cwe_id = alert['rule']['id'].split('-')[1]  # Extract CWE ID from rule ID
     cwe_url = f"https://cwe.mitre.org/data/definitions/{cwe_id}.html"
     cwe_response = requests.get(cwe_url)
     soup = BeautifulSoup(cwe_response.text, 'html.parser')
-    likelihood = soup.find('div', {'id': 'LikelihoodOfExploit'}).find_next('div').text.strip()
-    if likelihood.lower() == 'high':
-        print(f"Vulnerability: {alert['rule']['name']}, Severity: {alert['severity']}, CWE: {cwe_id}, Likelihood of Exploitability: {likelihood}")
+    likelihood_div = soup.find('div', {'id': 'Likelihood_Of_Exploit'})
+    if likelihood_div is not None:
+        likelihood = likelihood_div.find('div', {'class': 'detail'}).find(
+            'div', {'class': 'indent'}).text.strip()
+        if likelihood.lower() == 'high':
+            print(
+                f"Vulnerability: {alert['rule']['name']}, Severity: {alert['severity']}, CWE: {cwe_id}, Likelihood of Exploitability: {likelihood}")
+    else:
+        print(f"No 'Likelihood_Of_Exploit' found for CWE: {cwe_id}")
